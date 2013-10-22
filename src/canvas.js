4ea67b9cac72ea4ef6cc52c3103a9dfebcb67108
Norbert.staff.cmd <nmauger@nm-1129.staffandline.local>
2013-10-22 15:51:46 +0200

    +++ collision detection done

--- src/canvas.js
+++ src/canvas.js
@@ -1,8 +1,12 @@
 // TODO :
 // Graphics :
-// 0 - Draw the colony
-// 1 - ant must head in the right direction
-// 2 - Proper body circles (correct size)
+// 0 - Draw the colony => DONE
+// 1 - ant must head in the right direction => DONE
+// 2 - Proper body circles (correct size) => Done
+
+// 3 - Collision detection => Change direction if obstacle in front + test if obstacle == food.
+
+
 
 // Ant :
 // Is empty => No pheromons path created
@@ -42,41 +46,63 @@ $(document).ready(function(){
   */
 //BEGIN LIBRARY CODE
   
-  var NB_ANTS = 50; 
+  var NB_ANTS = 50;
+  var NB_FOODS = 10;
+  var NB_OBSTACLES = 10;
   
-  var BALL_SIZE = 10;
-  var HEAD_SIZE = 10;
-  var TORSO_SIZE = 10;
-  var MIDDLE_SIZE = 10;
-  var ABDOMEN_SIZE = 10;
+  var BLOCK_SIZE = 25;
+  var BALL_SIZE = 2;
+  var HEAD_SIZE = 2;
+  var TORSO_SIZE = 2;
+  var MIDDLE_SIZE = 2;
+  var ABDOMEN_SIZE = 2;
+  var FOOD_SIZE = 10;
+  var ROCK_SIZE = 20;
   
-  var sophie;
   var ants = [];
+  var foodReserves = [];
+  var obstacles = [];
   
-  var antsColor = ["#445500", "#553300", "#332200"];
+  var antsColor = ["#556622", "#775522", "#553311"];
   var groundColor = "#081200";
   
   var WIDTH;
   var HEIGHT;
   var ctx;
   
-  var ant = function() {
+  var Colony = function() {
+    this.x = Math.random() * WIDTH / 2;
+    this.y = Math.random() * HEIGHT / 2;
+    this.color = "#115511";
+    
+    this.draw = function () {
+      ctx.save();
+
+      ctx.fillStyle = this.color;
+      circle(this.x, this.y, BLOCK_SIZE);
+      circle(this.x+BLOCK_SIZE, this.y, BLOCK_SIZE);
+      circle(this.x-BLOCK_SIZE, this.y, BLOCK_SIZE);
+      circle(this.x, this.y - BLOCK_SIZE, BLOCK_SIZE);
+      
+      ctx.restore();
+    };
+  };
+  
+  var Ant = function() {
     this.x = Math.random() * WIDTH*0.90;
     this.y = Math.random() * HEIGHT*0.90;
-    this.dx = Math.random() * 10;
-    this.dy = Math.random() * 10;
-    this.angle = 0;
-    this.dAngle = 2.5;
+    this.dx = Math.random() * 4 - 2;
+    this.dy = Math.random() * 4 - 2;
+    this.speed = Math.random() * 1 + 1;
     
-    console.log((Math.floor(Math.random()*3)));
+    this.heading = Math.atan(this.dy / this.dx );
     
     this.color = antsColor[Math.floor((Math.random()*3))];
-    console.log(this.color);
     
     this.draw = function () {
       ctx.save();
       ctx.translate( this.x, this.y );
-      ctx.rotate(this.angle*Math.PI/180);
+      ctx.rotate(this.heading);
       ctx.translate( -this.x, -this.y );
 
       ctx.fillStyle = this.color;
@@ -88,29 +114,113 @@ $(document).ready(function(){
       this.angle += this.dAngle;
       
       ctx.restore();
-     
-
     };
     
     this.update = function () {
+      
+      // Border collision
       if (this.x + this.dx + BALL_SIZE > WIDTH || this.x + this.dx - BALL_SIZE < 0)
       {
         this.dx = -this.dx;
-        this.dAngle = -this.dAngle;
       }
+      
       if (this.y + this.dy + BALL_SIZE > HEIGHT || this.y + this.dy - BALL_SIZE < 0)
       {
         this.dy = -this.dy;
-        this.dAngle = -this.dAngle;
       }
-     
-      this.x += this.dx;
-      this.y += this.dy;
-      this.angle += this.dAngle;
+      
+      // Ants Collision
+      for (var i = 0 ; i < ants.length ; i++ ) {
+        if ( this.x < ants[i].x && this.x + this.dx + BALL_SIZE > ants[i].x - BALL_SIZE ) {
+          if ( this.y < ants[i].y && this.y + this.dy + BALL_SIZE > ants[i].y - BALL_SIZE ) {
+            this.dx = this.dx -2; 
+            this.dy = -this.dy; 
+          }
+        }
+      }
+        
+      // Colony collision
+      if ( (this.x + this.dx < colony.x && this.x + this.dx + BALL_SIZE > colony.x - BLOCK_SIZE*2) ||
+           (this.x + this.dx > colony.x && this.x + this.dx - BALL_SIZE < colony.x + BLOCK_SIZE*2) ) {
+        if ( this.y + this.dy < colony.y && this.y + this.dy + BALL_SIZE > colony.y - BLOCK_SIZE*2 ) {
+          this.dx = -this.dx; 
+          this.dy = -this.dy; 
+        }
+        else if ( this.y + this.dy > colony.y && this.y + this.dy - BALL_SIZE < colony.y + BLOCK_SIZE ) {
+          this.dx = -this.dx; 
+          this.dy = -this.dy; 
+        }
+
+      }
+          
+      // obstacles collision
+      for ( var i = 0 ; i < obstacles.length ; i++ ) {
+         
+        if ( (this.x + this.dx < obstacles[i].x && this.x + this.dx + BALL_SIZE > obstacles[i].x - ROCK_SIZE) ||
+             (this.x + this.dx > obstacles[i].x && this.x + this.dx - BALL_SIZE < obstacles[i].x + ROCK_SIZE) ) {
+          if ( this.y + this.dy < obstacles[i].y && this.y + this.dy + BALL_SIZE > obstacles[i].y - ROCK_SIZE ) {
+            this.dx = -this.dx; 
+            this.dy = -this.dy; 
+          }
+          else if ( this.y + this.dy > obstacles[i].y && this.y + this.dy - BALL_SIZE < obstacles[i].y + ROCK_SIZE ) {
+            this.dx = -this.dx; 
+            this.dy = -this.dy; 
+          }
+  
+        }
+      }
+
+      // Food collision
+      for ( var i = 0 ; i < foodReserves.length ; i++ ) {
+         
+        if ( (this.x + this.dx < foodReserves[i].x && this.x + this.dx + BALL_SIZE > foodReserves[i].x - FOOD_SIZE) ||
+             (this.x + this.dx > foodReserves[i].x && this.x + this.dx - BALL_SIZE < foodReserves[i].x + FOOD_SIZE) ) {
+          if ( this.y + this.dy < foodReserves[i].y && this.y + this.dy + BALL_SIZE > foodReserves[i].y - FOOD_SIZE ) {
+            this.dx = -this.dx; 
+            this.dy = -this.dy; 
+          }
+          else if ( this.y + this.dy > foodReserves[i].y && this.y + this.dy - BALL_SIZE < foodReserves[i].y + FOOD_SIZE ) {
+            this.dx = -this.dx; 
+            this.dy = -this.dy; 
+          }
+  
+        }
+      }
+      
+
+      this.x += this.dx * this.speed;
+      this.y += this.dy * this.speed;
+      
+      this.heading = Math.atan( this.dy / this.dx );
       
     };
   };
   
+  var FoodReserve = function () {
+    this.x = Math.random() * WIDTH * 0.85;
+    this.y = Math.random() * HEIGHT * 0.85;
+    this.color = "#CC3333";
+    
+    this.draw = function () {
+      ctx.save();
+      ctx.fillStyle = this.color;
+      circle(this.x, this.y, FOOD_SIZE);
+      ctx.restore();
+    };
+  };
+  
+  var Rock = function () {
+    this.x = Math.random() * WIDTH * 0.85;
+    this.y = Math.random() * HEIGHT * 0.85;
+    this.color = "#BBBBBB";
+    
+    this.draw = function () {
+      ctx.save();
+      ctx.fillStyle = this.color;
+      circle(this.x, this.y, ROCK_SIZE);
+      ctx.restore();
+    };
+  };
    
   function init() {
     ctx = $('#canvas')[0].getContext("2d");
@@ -120,9 +230,18 @@ $(document).ready(function(){
     HEIGHT = $("#canvas").height();
     
     for ( var i = 0 ; i < NB_ANTS ; i++ ) {
-      ants.push(new ant());
+      ants.push(new Ant());
     }
-    sophie = new ant();
+    
+    for ( var i = 0 ; i < NB_FOODS ; i++ ) {
+      foodReserves.push(new FoodReserve());
+    }
+
+    for ( var i = 0 ; i < NB_OBSTACLES ; i++ ) {
+      obstacles.push(new Rock());
+    }
+    colony = new Colony();
+    
     return setInterval(draw, 10);
     
   }
@@ -152,15 +271,22 @@ $(document).ready(function(){
    
   function draw() {
   
-    sophie.update();
     clear();
-    sophie.draw();
+
+    colony.draw();
     
     for ( var i = 0 ; i < ants.length ; i++ ) {
       ants[i].update();
       ants[i].draw();
     }
     
+    for ( var i = 0 ; i < foodReserves.length ; i++ ) {
+      foodReserves[i].draw();
+    }
+
+    for ( var i = 0 ; i < obstacles.length ; i++ ) {
+      obstacles[i].draw();
+    }
    
   }
    
