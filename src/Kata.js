3b94d96351f005bcf5547936c3a9e78e4bccfc0e
norbert <norbert.mauger@gmail.com>
2013-11-05 18:04:43 +0100

    correct some jslint problem + new inspection profile

--- src/Kata.js
+++ src/Kata.js
@@ -1,27 +1,33 @@
 function Kata() {
-  
+  'use strict';
 }
 
-Kata.prototype.calculateMinimumValue = function ( list ) {
+Kata.prototype.calculateMinimumValue = function (list) {
+  'use strict';
   list.sort();
-  return list[0]; 
-}
+  return list[0];
+};
 
-Kata.prototype.calculateMaximumValue = function ( list ) {
+Kata.prototype.calculateMaximumValue = function (list) {
+  'use strict';
   list.sort();
-  return list[list.length-1]; 
-}
+  return list[list.length - 1];
+};
 
-Kata.prototype.calculateSequenceLength = function ( list ) {
-  return list.length; 
-}
+Kata.prototype.calculateSequenceLength = function (list) {
+  'use strict';
+  return list.length;
+};
 
-Kata.prototype.calculateAverageValue = function ( list, precision ) {
-  var sum = 0;
-  for ( var i = 0 ; i < list.length ; i++ ) {
+Kata.prototype.calculateAverageValue = function (list, precision) {
+  'use strict';
+  var sum = 0,
+    i,
+    prec;
+  for (i = 0; i < list.length; i++) {
     sum += list[i];
   }
-  
+
   prec = Math.pow(10, precision);
-  return (Math.round(parseFloat(sum/list.length)*prec)/prec)
-}
+  return (Math.round(parseFloat(sum / list.length) * prec) / prec);
+};
